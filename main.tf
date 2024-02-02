bab5545359d0df5960815da0b80a7b205ec34242
LuzSerranoDiaz <diaz03luz@gmail.com>
2024-02-02 11:55:09 +0000

    ye

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "sg_frontend_terraform" {
 
 # Creamos las reglas de entrada del grupo de seguridad. 
 # Utilizamos un bucle para recorrer la lista de puertos definida como variable
-resource "aws_security_group_rule" "ingress" {
+resource "aws_security_group_rule" "ingress1" {
   security_group_id = aws_security_group.sg_frontend_terraform.id
   type              = "ingress"
 
@@ -26,17 +26,16 @@ resource "aws_security_group_rule" "ingress" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
-resource "aws_security_group_rule" "ingress" {
-  security_group_id = aws_security_group.sg_frontend_terraform
-  type              = "ingress"
-
+resource "aws_security_group_rule" "ingress2" {
+  security_group_id = aws_security_group.sg_frontend_terraform.id
+  type        = "ingress"
   from_port   = 0
   to_port     = 0
   protocol    = "ICMP"
   cidr_blocks = ["0.0.0.0/0"]
 }
-resource "aws_security_group_rule" "egress" {
-  security_group_id = aws_security_group.sg_frontend_terraform
+resource "aws_security_group_rule" "egress1" {
+  security_group_id = aws_security_group.sg_frontend_terraform.id
   type              = "egress"
 
   from_port   = 0
@@ -46,18 +45,18 @@ resource "aws_security_group_rule" "egress" {
 }
 
 #Backend
-resource "aws_security_group_rule" "ingress" {
+resource "aws_security_group_rule" "ingress3" {
   security_group_id = aws_security_group.sg_backend_terraform.id
   type              = "ingress"
 
   count       = length(var.allowed_ingress_ports_backend)
-  from_port   = var.var.allowed_ingress_ports_backend[count.index]
-  to_port     = var.var.allowed_ingress_ports_backend[count.index]
+  from_port   = var.allowed_ingress_ports_backend[count.index]
+  to_port     = var.allowed_ingress_ports_backend[count.index]
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
-resource "aws_security_group_rule" "ingress" {
-  security_group_id = aws_security_group.sg_backend_terraform
+resource "aws_security_group_rule" "ingress4" {
+  security_group_id = aws_security_group.sg_backend_terraform.id
   type              = "ingress"
 
   from_port   = 0
@@ -65,8 +64,8 @@ resource "aws_security_group_rule" "ingress" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
-resource "aws_security_group_rule" "ingress" {
-  security_group_id = aws_security_group.sg_backend_terraform
+resource "aws_security_group_rule" "ingress5" {
+  security_group_id = aws_security_group.sg_backend_terraform.id
   type              = "ingress"
 
   from_port   = 0
@@ -75,8 +74,8 @@ resource "aws_security_group_rule" "ingress" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "egress" {
-  security_group_id = aws_security_group.sg_backend_terraform
+resource "aws_security_group_rule" "egress2" {
+  security_group_id = aws_security_group.sg_backend_terraform.id
   type              = "egress"
 
   from_port   = 0
@@ -111,9 +110,9 @@ resource "aws_instance" "instancia_terraform_backend" {
 
 # Creamos una IP elástica y la asociamos a la instancia
 resource "aws_eip" "ip_elastica_backend" {
-  instance = aws_instance.instancia_terraform_backend
+  instance = aws_instance.instancia_terraform_backend.id
 }
 
 resource "aws_eip" "ip_elastica_frontend" {
-  instance = aws_instance.instancia_terraform_frontend
+  instance = aws_instance.instancia_terraform_frontend.id
 }
